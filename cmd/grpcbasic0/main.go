7f80ab228f46c134cbe9aad874f394b1cdceb9bf
Daved <daved@codemodus.com>
2018-07-05 16:40:00 -0700

    Move listener func below main, correct error formatting.

--- cmd/grpcbasic0/main.go
+++ cmd/grpcbasic0/main.go
@@ -10,20 +10,6 @@ import (
 	"google.golang.org/grpc"
 )
 
-func listener(port string) (*net.TCPListener, error) {
-	a, err := net.ResolveTCPAddr("tcp", port)
-	if err != nil {
-		return nil, fmt.Errorf("cannot resolve addr: %v\n", err)
-	}
-
-	l, err := net.ListenTCP("tcp", a)
-	if err != nil {
-		return nil, fmt.Errorf("cannot listen: %v\n", err)
-	}
-
-	return l, nil
-}
-
 func main() {
 	var port string
 	flag.StringVar(&port, "rcp", ":3323", "rcp port (default: ':3323')")
@@ -31,7 +17,7 @@ func main() {
 
 	l, err := listener(port)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "cannot get listener: %v\n", err)
+		fmt.Fprintf(os.Stderr, "cannot get listener: %s\n", err)
 		os.Exit(1)
 	}
 
@@ -39,7 +25,21 @@ func main() {
 	pb.RegisterUserServiceServer(srvr, svc)
 
 	if err = srvr.Serve(l); err != nil {
-		fmt.Fprintf(os.Stderr, "rpc server error: %v\n", err)
+		fmt.Fprintf(os.Stderr, "rpc server error: %s\n", err)
 		os.Exit(1)
 	}
+}
+
+func listener(port string) (*net.TCPListener, error) {
+	a, err := net.ResolveTCPAddr("tcp", port)
+	if err != nil {
+		return nil, fmt.Errorf("cannot resolve addr: %s", err)
+	}
+
+	l, err := net.ListenTCP("tcp", a)
+	if err != nil {
+		return nil, fmt.Errorf("cannot listen: %s", err)
+	}
+
+	return l, nil
 }
